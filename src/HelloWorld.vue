ea42cc092c090f988f2c1c960daa474644d6331c
X <2457981708@qq.com>
2024-11-05 13:44:41 +0800

    commit msg

---
+++ src/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="hello">
+      <h1>Hello Vue</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld'
+  }
+  </script>
+  
+  <style scoped>
+  #hello {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
